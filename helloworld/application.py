a6f967c6e6103d16167975d510a952f696c3da58
unknown <kwakwoohyun010@gmail.com>
2020-03-12 16:42:49 +0900

    fix: Update application.py v3

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,19 +1,26 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+from flask import Flask, Response, json, request
 from helloworld.flaskrun import flaskrun
+import googletrans
+from googletrans import Translator
 
 application = Flask(__name__)
 
 
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World hihi'}), mimetype='application/json', status=200)
 
 
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+
+@application.route("/language")
+def language():
+    return googletrans.LANGUAGES
 
 
 if __name__ == '__main__':
